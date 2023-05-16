2659e8f1f3f53dea46f89a3ce2776838a28c1b65
ajkn1904 <ajknishat@gmail.com>
2023-05-17 00:09:36 +0600

    header added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/Components/Navbar'
+import Header from '@/Components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`min-h-screen overflow-hidden font-serif ${inter.className} max-w-[1440px] mx-auto`}
+      className={`min-h-screen font-serif ${inter.className} max-w-[1440px] mx-auto`}
     >
 
       <Navbar />
+
+      <Header/>
 
     </main>
   )
